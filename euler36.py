b8ae22a15b80edd99ff625c67957bac411f7c9c7
Satima2002 <63864609+Satima2002@users.noreply.github.com>
2022-03-03 18:13:12 +0200

    Add files via upload

---
+++ euler36.py
@@ -0,0 +1,27 @@
+import math
+def checkIfPolindrom(numb):
+		number=str(numb)
+		s=0
+		if(len(number)%2==0):
+			for j in range(int(len(number)/2)):
+				if(number[j]==number[len(number)-1-j]):
+					s+=1
+			if(s==(len(number)/2)):
+				return True
+			else:
+				return False
+		else:
+			s+=1
+			for j in range(int(len(number)/2)):
+				if(number[j]==number[len(number)-1-j]):
+					s+=1
+			if(s==math.ceil(len(number)/2)):
+				return number
+			else:
+				return False
+sum=0
+for j in range(1000000):
+	y = bin(j)[2:]
+	if(checkIfPolindrom(j) and checkIfPolindrom(y)):
+		sum+=j
+print(sum)
