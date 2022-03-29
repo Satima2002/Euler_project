fe8e5728735afd8c1dd426ddb8213abd3ca14d72
Satima Valsony <63864609+Satima2002@users.noreply.github.com>
2022-03-30 00:15:21 +0300

    Update euler10.py

--- euler10.py
+++ euler10.py
@@ -1,14 +1,25 @@
-simple=[]
-for i in range(2000000):
-    temp=[]
-    for j in range(1,i):
-        if i%j==0:
-            temp.append(j)
-           # j+=1
-    if len(temp)<2:
-        simple.append(i)
-del simple[1]
-del simple[0]
-print(sum(simple))
+def is_prime(n):
+    """
+    Assumes that n is a positive natural number
+    """
+    # We know 1 is not a prime number
+    if n == 1:
+        return False
 
-# блин код правильный но интерпретатор работает медленно и поэтому почти невозможно без знания машинного обучения
+    i = 2
+    # This will loop from 2 to int(sqrt(x))
+    while i*i <= n:
+        # Check if i divides x without leaving a remainder
+        if n % i == 0:
+            # This means that n has a factor in between 2 and sqrt(n)
+            # So it is not a prime number
+            return False
+        i += 1
+    # If we did not find any factor in the above loop,
+    # then n is a prime number
+    return True
+sum=0
+for i in range(2000000):
+    if (is_prime(i)):
+        sum+=i
+print(sum)
